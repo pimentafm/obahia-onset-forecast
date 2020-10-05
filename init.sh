0ff9db1523b2d391d0c6df291593ccf2fdd50915
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-10-05 13:41:14 -0300

    Bug fixes

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# ./step_01_grb2netcdf.sh
-# ./step_02_soma_timestep.sh
-# ncl step_03_calculo_inicio_estac_chuva.ncl
-# ./step_04_agrupa_dia.sh
-# ./step_05_agrupa_20ensemble.sh
-# python3.7 step_06_mean_onset.py
-# ./step_07_norientation.sh
+./step_01_grb2netcdf.sh
+./step_02_soma_timestep.sh
+ncl step_03_calculo_inicio_estac_chuva.ncl
+./step_04_agrupa_dia.sh
+./step_05_agrupa_20ensemble.sh
+python3.7 step_06_mean_onset.py
+./step_07_norientation.sh
 grass76  --tmp-location utils/region_reference.tif --exec bash step_08_interp2pg.sh
